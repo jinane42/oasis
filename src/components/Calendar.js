610fee19232abbb459940065b8e257a92a6d5c6f
jinane42 <jinane.ouzina@gmail.com>
2021-09-09 16:58:50 +0200

    api calendar working

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,30 +1,71 @@
-
+import React, { useState, useEffect, Component } from 'react';
 import '../style/Calendar.css'
-import '@natscale/react-calendar/dist/main.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import React, { useState, useEffect, useCallback } from 'react';
-import {Calendar, momentLocalizer} from 'react-big-calendar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+
 
 const localizer = momentLocalizer(moment)
 const axios = require('axios')
 
 
+class Cal extends React.Component {
 
-axios.get('https://djangoeventsapi.herokuapp.com/api/events')
-.then(function(response){
-  console.log(response)
-  
-})
+  constructor(props) {
+    super(props)    
+    this.state = {
+      cal_events: [],
+    }
+  }
 
-const Call = props => (
-  <div className="calContainer">
-    <Calendar
-      localizer={localizer}
-      events={[{title: 'jinane', start : new Date('2021-09-09T09:30:00'), end : new Date('2021-09-09T17:30:00'), allDay: false, ressource: {}}]}
-      startAccessor="start"
-      endAccessor="end"
-      />
-  </div>
-)
-export default Call
+
+  convertDate = (date) => {
+    return moment.utc(date).toDate()
+  }
+
+  componentDidMount() {
+
+    axios.get('https://djangoeventsapi.herokuapp.com/api/events/')
+      .then(response => {
+        console.log(response.data)
+        let eventArray =  []
+        let appointments = response.data;
+        
+        appointments.forEach(element => {
+          let newEvent = {title: element.name, start: new Date(element.start_date), end: new Date(element.end_date), allDay: false, ressource: 'any'}
+          eventArray.push(newEvent)
+        });
+
+        console.log(appointments)
+        this.setState({
+          cal_events: eventArray
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    
+  }
+
+
+
+  render() {
+
+    const { cal_events } = this.state
+    console.log(cal_events)
+
+    return (
+      <div className="calContainer">
+        <Calendar
+          localizer={localizer}
+          events={cal_events}
+          startAccessor="start"
+          endAccessor="end"
+
+        />
+      </div>
+    )
+  }
+
+}
+export default Cal;
